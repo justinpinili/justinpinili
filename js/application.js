d96efb2d7e72cda419aa0667ba73fc06bd5e67ad
Justin Pinili <pinili.justin@gmail.com>
2014-03-25 00:45:25 -0700

    added imagesloaded dependancy for masonry

--- js/application.js
+++ js/application.js
@@ -1,23 +1,12 @@
-var pictureList = new PictureList();
-var pictureListView;
-var container = document.querySelector('#container');
-var msnry;
-var count = 0;
+var $container = $('#container');
 
 $(document).ready(function() {
   $(function(){ SurfWallApp.start() });
 
   SurfWallApp.pictureList.fetch().done(function(){
     SurfWallApp.pictureListView.render();
-  });
-
-  $('<img />').load( function(){
-    imagesLoaded( container, function() {
-      msnry= new Masonry( container, {
-      // options
-      columnWidth: 250,
-      itemSelector: '.item'
-      });
+    imagesLoaded( '#container', function() {
+      $container.masonry();
     });
   });
 });
